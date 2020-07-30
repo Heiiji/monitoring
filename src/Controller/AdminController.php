9385dd788121fccd6d0bbf1b361957c56ce0fd79
Julien <julien.juret@gmail.com>
2020-07-31 00:15:43 +0200

    ajout de l'espace admin

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\WebsiteRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin_dashboard")
+     */
+    public function index(WebsiteRepository $repository)
+    {
+        $websites = $repository->findAll();
+        return $this->render('admin/index.html.twig', [
+            'websites' => $websites
+        ]);
+    }
+}
