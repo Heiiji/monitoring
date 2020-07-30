6640f4fc093d6f12756df24eb31e2aa39891642d
Julien <julien.juret@gmail.com>
2020-07-30 11:07:24 +0200

    Ajout de la fiche du site

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Website;
 use App\Repository\WebsiteRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,6 +18,15 @@ class HomeController extends AbstractController
 
         return $this->render('home/index.html.twig', [
             'websites' => $websites
+        ]);
+    }
+    
+    /**
+     * @Route("/websites/{id}", name="website_show")
+     */
+    public function show(Website $website) {
+        return $this->render('home/show.html.twig', [
+            'website' => $website
         ]);
     }
 }
